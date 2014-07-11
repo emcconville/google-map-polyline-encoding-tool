bac0dd1c84e23cfe8c25dd826c47a07678fb5607
Martin Lindhe <martin.lindhe@freespee.com>
2014-07-11 12:38:57 +0200

    whitespace: convert mixed case (tabs and spaces) to spaces

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,12 +1,12 @@
 <?php
 // Try to load distributed library
 if(file_exists($file = __DIR__.'/../dist/Polyline.php')) {
-	print "Loading distributed library\n";
+    print "Loading distributed library\n";
     require_once $file;
 // Else, attempt to load library source
 } elseif(file_exists($file = __DIR__.'/../src/Polyline.php')) {
-	print "Loading library source\n";
-	require_once $file;
+    print "Loading library source\n";
+    require_once $file;
 } else { // Panic and die
-	exit(1);
+    exit(1);
 }
