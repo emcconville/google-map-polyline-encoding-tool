84d90bcf7f722fe75f0084e940048372a29493dc
Eric McConville <_@emcconville.com>
2014-08-08 10:13:49 -0500

    Lint-ed test file

--- tests/Issue10Test.php
+++ tests/Issue10Test.php
@@ -8,11 +8,11 @@ class Issue10 extends PHPUnit_Framework_TestCase
 {
   public function testRounding()
   {
-    $original_points = array(48.000006, 2.000004,48.00001,2.00000);
-    $encoded = Polyline::Encode($original_points);
-    $this->assertEquals('a_~cH_seK??',$encoded);
-    $decoded_points = Polyline::Decode($encoded);
-    $this->assertTrue($decoded_points[0] === $decoded_points[2]);
-    $this->assertTrue($decoded_points[1] === $decoded_points[3]);
+    $originalPoints = array(48.000006, 2.000004,48.00001,2.00000);
+    $encoded = Polyline::Encode($originalPoints);
+    $this->assertEquals('a_~cH_seK??', $encoded);
+    $decodedPoints = Polyline::Decode($encoded);
+    $this->assertTrue($decodedPoints[0] === $decodedPoints[2]);
+    $this->assertTrue($decodedPoints[1] === $decodedPoints[3]);
   }
 }
