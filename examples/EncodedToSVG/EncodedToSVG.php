043f1ff71a0c25514dbf7e9e11ff5b8695d16165
Eric McConville <emcconville@emcconville.com>
2014-08-21 20:53:40 -0500

    Fixed some light docs, and added LGPL header

--- examples/EncodedToSVG/EncodedToSVG.php
+++ examples/EncodedToSVG/EncodedToSVG.php
@@ -1,4 +1,29 @@
 <?php
+/**
+ * Encoded String to SVG example
+ *
+ * An example class to convert encoded strings to SVG for sample viewing.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package EncodedToSVG
+ * @author E. McConville <emcconville@emcconville.com>
+ * @copyright 2014 emcconville
+ * @license GNU Lesser General Public License <http://www.gnu.org/licenses/lgpl.html>
+ * @link https://github.com/emcconville/google-map-polyline-encoding-tool
+ * @since v1.2.4
+ */
   
 class EncodedToSVG extends Polyline
 {
@@ -6,18 +31,21 @@ class EncodedToSVG extends Polyline
    * Decode string and generate a full SVG document.
    *
    * @uses DOMDocument
-   * @param string $encoded - Encoded poyline
+   * @param string $encoded - Encoded polyline
    * @return string - SVG document
    */
   public static function DecodeToSVG($encoded)
   {
+    // Create list of points
     $points = parent::Decode($encoded);
+    // Grab first pair
     list($x, $y) = self::shiftPoint($points);
+    // Path will need to start by moving to first coordinate.
     $path = sprintf('M %f %f L ', $x, $y);
-    // Init bounding box
+    // Init bounding box's min & max.
     $minX = $maxX = $x;
     $minY = $maxY = $y;
-    while ( $points ) {
+    while ( $points ) { // This can be simplified with php-5.5's generators.
       list($x, $y) = self::shiftPoint($points);
       $path .= sprintf('%f %f, ', $x, $y);
       // Grow MBR
@@ -28,32 +56,9 @@ class EncodedToSVG extends Polyline
     }
     // Close poylgon
     $path = rtrim($path, ', ') . ' Z';
-    
-    // Build XML
-    $dom = new DOMDocument('1.0', 'utf-8');
-    $dom->formatOutput = true;
-    // Root
-    $root = $dom->createElementNS('http://www.w3.org/2000/svg', 'svg');
-    $root->appendChild(new DomAttr('version', '1.2'));
+    // Create viewBox from MBR points
     $mbr =  sprintf("%f %f %f %f", $minX, $minY, abs($maxX - $minX), abs($maxY - $minY));
-    $root->appendChild(new DomAttr('viewBox', $mbr));
-    $root->appendChild(new DomAttr('viewport-fill', 'lightblue'));
-    $root->appendChild(new DomAttr('style', 'background-color:lightblue;'));
-    // Group
-    $g = $dom->createElement('g');
-    $g->appendChild(new DomAttr('stroke', 'rgba(0,0,0,0.5)'));
-    $g->appendChild(new DomAttr('stroke-width', '0.25%'));
-    $g->appendChild(new DomAttr('fill', 'beige'));
-    // Path
-    $p = $dom->createElement('path');
-    $p->appendChild(new DomAttr('d', $path));
-    
-    // Pull it all together
-    $g->appendChild($p);
-    $root->appendChild($g);
-    $dom->appendChild($root);
-    
-    return $dom->saveXML();
+    return self::generateSVG($path, $mbr);
   }
 
   /**
@@ -70,5 +75,40 @@ class EncodedToSVG extends Polyline
     $y = array_shift($points);
     $x = array_shift($points);
     return array( $x, $y * -1 );
+  }
+
+  /**
+   * Turn path & MBR into a valid SVG string
+   *
+   * @param string $pathData
+   * @param string $viewBox
+   * @return string - SVG
+   */
+  private static function generateSVG($pathData, $viewBox)
+  {
+    // Build XML
+    $dom = new DOMDocument('1.0', 'utf-8');
+    $dom->formatOutput = true;
+    // Root
+    $root = $dom->createElementNS('http://www.w3.org/2000/svg', 'svg');
+    $root->appendChild(new DomAttr('version', '1.2'));
+    $root->appendChild(new DomAttr('viewBox', $viewBox));
+    $root->appendChild(new DomAttr('viewport-fill', 'lightblue'));
+    $root->appendChild(new DomAttr('style', 'background-color:lightblue;'));
+    // Group
+    $g = $dom->createElement('g');
+    $g->appendChild(new DomAttr('stroke', 'rgba(0,0,0,0.5)'));
+    $g->appendChild(new DomAttr('stroke-width', '0.25%'));
+    $g->appendChild(new DomAttr('fill', 'beige'));
+    // Path
+    $p = $dom->createElement('path');
+    $p->appendChild(new DomAttr('d', $pathData));
+  
+    // Pull it all together
+    $g->appendChild($p);
+    $root->appendChild($g);
+    $dom->appendChild($root);
+  
+    return $dom->saveXML();
   }
 }
